1b06c7c78080a7c97ec88ee12cc3b5597c8cac31
eduardonk9999 <thiago.novak7@gmail.com>
2022-01-17 17:43:04 -0300

    Add, project

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import { Login } from "./components/Login"
+import { ProtectedLayout } from "./components/ProtectedLayout"
+import { AuthProvider } from "./context/AuthProvider"
+
+function App() {
+  return (
+   <AuthProvider>
+     <BrowserRouter>
+      <Switch>
+        <Route path="/profile">
+          <ProtectedLayout>
+            <h2>Olá Sou o Profile</h2>
+          </ProtectedLayout>
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
+     </BrowserRouter>
+   </AuthProvider>
+  )
+}
+
+export default App
